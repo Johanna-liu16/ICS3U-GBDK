3614751c3cdaeba278da123d13c8fc98dc08d6c0
Johanna Liu <johanna.liu@mths.ca>
2023-01-13 20:04:09 +0000

    all new files

---
+++ lesson-01/main.c
@@ -0,0 +1,12 @@
+// Copyright Johanna Liu 2022
+//
+// Created by Johanna Liu
+// Created in Jan 2023
+// This program is for Space Aliens on GameBoy
+
+#include <gb/gb.h>
+#include <stdio.h>
+
+void main(){
+    printf("Hello, World!");
+}
