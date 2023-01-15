5421243e3e16dc0d849fa87b2870c40fe6b4fd41
Johanna Liu <johanna.liu@mths.ca>
2023-01-15 00:52:36 +0000

    lesson 5

--- lesson-05/main.c
+++ lesson-05/main.c
@@ -7,13 +7,16 @@
 #include <gb/gb.h>
 #include <stdio.h>
 
+#include "GBDK_Constants.h"
 #include "MetaSprites.c"
 #include "SpaceAliens.c"
 #include "Background.c"
 
 void main(){
-    // user input
-    uint8_t joypadData;
+    // keep track of meta sprite position, in a variable
+    const int FIXED_Y_POSITION_OF_SHIP = 144;
+    int shipXPosition = 76;
+    int shipYPosition = FIXED_Y_POSITION_OF_SHIP;
 
     // load spritesheet referenced as #0
     // load 16 sprites from it
@@ -23,11 +26,11 @@ void main(){
     // load sprites for eta sprite
     set_meta_sprite_tile(0, 1, 2, 3, 4);
 
-    // move sprite indexed #0 to (88, 78)
-    move_meta_sprite(0, 76, 120);
+    // move meta sprite on screen
+    move_meta_sprite(0, shipXPosition, shipYPosition);
 
-    //set background tile sheet 0 to a different tile sheet
-    set_bkg_data(0, 43, SpaceAliens);
+    //set background tile sheet 0 to the same tile sheet
+    set_bkg_data(0, 16, SpaceAliens);
     // load tiles from (0, 0) to (20, 36) form the tile map called background
     set_bkg_tiles(0, 0, 20, 36, Background);
 
@@ -38,32 +41,28 @@ void main(){
     // game loop
     while (1) {
         // get D-pad input
-        joypadData = joypad();
-
-        // If LEFT D-pad is pressed
-        if (joypadData & J_LEFT) {
-            // move sprite in teh index 0 by (x,y) (-1, 0)
-            scroll_meta_sprite(0, -1, 0);
+        switch (joypad()) {
+            case J_LEFT:
+                // move sprite in the index 0 by (x, y) (-1, 0)
+                scroll_meta_sprite(0, -1, 0);
+                shipXPosition -= 1;
+                if (shipXPosition < 8) {
+                    shipXPosition = 8;
+                    move_meta_sprite(0, 8, FIXED_Y_POSITION_OF_SHIP);
+                }
+                break;
+            case J_RIGHT:
+                // move sprite in teh index 0 by (x, y) (+1, 0)
+                scroll_meta_sprite(0, 1, 0);
+                shipXPosition += 1;
+                if (shipXPosition > SCREEN_WIDTH -8) {
+                    shipXPosition = SCREEN_WIDTH -8;
+                    move_meta_sprite(0, (uint8_t)(SCREEN_WIDTH - 8), FIXED_Y_POSITION_OF_SHIP);
+                }
+                break;
+            default:
+                break;
         }
-
-        // If RIGHT D-pad is pressed
-        if (joypadData & J_RIGHT) {
-            // move sprite in the index 0 by (x, y) (+1, 0)
-            scroll_meta_sprite(0, 1, 0);
-        }
-
-        // if UP D-pad is pressed
-        if (joypadData & J_UP) {
-            // move spritein the index 0 by (x,y) (0, -1)
-            scroll_meta_sprite(0, 0, -1);
-        }
-
-        //if DOWN D-pad is pressed
-        if (joypadData & J_DOWN) {
-            // move sprite in teh index 0 by (x, y)(0, +1)
-            scroll_meta_sprite(0, 0, 1);
-        }
-
         // scroll background 0 in the x and -1 in teh y
         scroll_bkg(0, -1);
 
